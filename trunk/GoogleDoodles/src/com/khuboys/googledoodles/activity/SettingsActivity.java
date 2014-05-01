d44823271b533a92d3d03a6c7b5200881c957d2f
huuquoc09@gmail.com <huuquoc09@gmail.com@2a827d85-c0b4-105e-5a07-5e88005b3297>
2014-05-01 14:25:01 +0000

    add reference screen & json parser

---
+++ trunk/GoogleDoodles/src/com/khuboys/googledoodles/activity/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.khuboys.googledoodles.activity;
+
+import com.khuboys.activity.googledoodles.R;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
---
+++ trunk/GoogleDoodles/src/com/khuboys/googledoodles/utils/Commons.java
@@ -0,0 +1,5 @@
+package com.khuboys.googledoodles.utils;
+
+public class Commons {
+
+}
